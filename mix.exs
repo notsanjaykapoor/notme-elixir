02e170be7fb8aa97912ea4d3b2b497a5cf0220ef
Sanjay Kapoor <notsanjaykapoor@gmail.com>
2024-01-21 15:33:51 -0600

    fix: revert docker entrypoint

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule Notme.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:ex_ulid, "~> 0.1.0"},
       {:broadway_kafka, "~> 0.4"},
-      {:faker, "~> 0.17", only: [:dev, :test]},
+      {:faker, "~> 0.17"},
       {:libcluster, "~> 3.3"},
       {:opentelemetry_exporter, "~> 1.3"}, # add this before other opentelemetry libs
       {:opentelemetry, "~> 1.2"},
