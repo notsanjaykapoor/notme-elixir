8984ee64e34b0fc8e640f1da1743b6f625b7fb6e
Sanjay Kapoor <notsanjaykapoor@gmail.com>
2024-01-27 09:42:13 -0600

    feat: added letsencrypt and install readme

--- lib/notme_web/controllers/weather_controller.ex
+++ lib/notme_web/controllers/weather_controller.ex
@@ -1,8 +1,6 @@
 defmodule NotmeWeb.WeatherController do
   use NotmeWeb, :controller
 
-  alias Notme.Service
-
   def index(conn, params) do
     city = Map.get(params, "city", "")
 
@@ -31,30 +29,6 @@ defmodule NotmeWeb.WeatherController do
         render(conn, :index, %{city: city, name: nil})
       _ ->
         render(conn, :index, %{city: city, name: nil})
-    end
-  end
-
-  def create(conn, params) do
-    user_handle = Map.get(params, "user_handle")
-    user_password = Map.get(params, "user_password")
-    user_id = :rand.uniform(100)
-
-    return_to = get_session(conn, :return_to) || "/"
-
-    case Service.Login.password_validate(user_password, Application.get_env(:notme, :auth)) do
-      {:ok, reason} ->
-        dbg(reason)
-        conn
-        |> delete_session(:return_to)
-        |> put_session(:user_id, user_id)
-        |> put_session(:user_handle, user_handle)
-        |> redirect(to: return_to)
-        |> halt()
-      {:error, reason} ->
-        dbg(reason)
-        conn
-        |> redirect(to: "/login")
-        |> halt()
     end
   end
 
